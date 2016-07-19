ab6dfba2fc7b82a730610319376482a4d9f82c2a
Maryia Kabash <maryia.kabash@itechart-group.com>
2016-07-19 11:55:15 +0300

    Fixing Heroku deployment -2

--- server.js
+++ server.js
@@ -16,13 +16,6 @@ app.use(function *(){
     this.body = 'Hello Pokemon Shop';
 });
 
-app.use(function* (next) {
-    // skip the rest of the code if the route does not match
-    if (this.request.path !== '/') return yield next;
-
-    this.response.body = 'we are at home!';
-});
-
 var port = 3000;
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
