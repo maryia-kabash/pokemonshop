31aeae8a26eb33ad7e3005769bb685feb0aa7f8c
Maryia Kabash <maryia.kabash@itechart-group.com>
2016-07-19 17:03:03 +0300

    Fixing Heroku deployment -8

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
-var http = require("http"),
-    port = process.env.PORT || 1881;
+// var http = require("http"),
+//     port = process.env.PORT || 1881;
+//
+// var server = http.createServer(function(request,response){
+//     response.writeHeader(200, {"Content-Type": "text/plain"});
+//     response.write("Hello HTTP!");
+//     response.end();
+// });
+//
+// let add = (x,y) => x + y;
+// let result = add(1,1);
+// console.log(result);
+//
+// server.listen(port);
+// console.log("Server Running on "+port+".\nLaunch http://localhost:"+port);
 
-var server = http.createServer(function(request,response){
-    response.writeHeader(200, {"Content-Type": "text/plain"});
-    response.write("Hello HTTP!");
-    response.end();
+var koa = require('koa');
+var app = koa();
+
+app.use(function *(){
+    this.body = 'Hello World!';
 });
-
-server.listen(port);
-console.log("Server Running on "+port+".\nLaunch http://localhost:"+port);
+console.log("Server Running on http://localhost:3000");
+app.listen(3000);
